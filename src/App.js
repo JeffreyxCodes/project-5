c978184d8b6a88e8b33b266bbd329e3e91234b1c
jeffreyxdc <jeffreyxdc@gmail.com>
2019-05-30 14:30:11 -0400

    added some accessibility to my charts, and voting now works

--- src/App.js
+++ src/App.js
@@ -7,14 +7,39 @@ class App extends Component {
     super();
 
     this.state = {
-      polls: [],
+      // array of objects, each of which contains a unique key and the poll object
+      polls: [], 
       isLoading: true,
     }
+  }
+
+  addVote = (chart, key, votes, choiceIndex) => {
+    const dbRef = firebase.database().ref(key);
+
+    const newVotes = votes;
+    newVotes[choiceIndex]++;
+
+    dbRef.child('/votes/').set(newVotes);
+
+    const newPolls = [...this.state.polls].map(poll => {
+      if (poll.key === key) {
+        poll.votes = newVotes;
+      }
+      return poll;
+    });
+  
+    // setting a new poll so that react will update
+    this.setState({
+      polls: newPolls
+    });
+
+    chart.update();
   }
 
   componentDidMount() {
     const dbRef = firebase.database().ref();
 
+    // with this ref, firebase will only react when change happens at the root
     dbRef.on('value', response => {
       const newPolls = [];
       const polls = response.val();
@@ -39,8 +64,13 @@ class App extends Component {
         {
           this.state.isLoading
             ? <h2>Loading...</h2>
-            : this.state.polls.map(poll => {
-              return <Poll key={poll.key} poll={poll.poll} />
+            : this.state.polls.map(pollObject => {
+              return <Poll 
+                        key={pollObject.key}
+                        id={pollObject.key}
+                        poll={pollObject.poll} 
+                        addVote={this.addVote}
+                      />
             })
         }
       </div>
--- src/Poll.js
+++ src/Poll.js
@@ -1,40 +1,110 @@
 import React, { Component } from 'react';
 import Chart from 'chart.js';
+import pattern from 'patternomaly';
 
 class Poll extends Component {
   constructor(props) {
     super(props);
     this.chartRef = React.createRef();
+    this.chart = undefined;
   }
 
   componentDidMount() {
     const myChartRef = this.chartRef.current.getContext("2d");
 
-    const chart = new Chart(myChartRef, {
-      type: "bar",
+    Chart.defaults.global.defaultFontColor = 'black';
+    // Chart.defaults.global.defaultFontSize = '16';
+    this.chart = new Chart(myChartRef, {
+      type: "horizontalBar",
       data: {
         labels: this.props.poll.choiceNames,
         datasets: [
           {
-            label: "",
-            data: this.props.poll.votes
-          }
+            label: '',
+            data: this.props.poll.votes,
+            borderWidth: 3,
+            backgroundColor: [
+              pattern.draw('weave', 'rgba(152,251,152, 0.6)'),
+              pattern.draw('diagonal', 'rgba(135,206,250, 0.6)'),
+              pattern.draw('dot', 'rgba(221,160,221, 0.6)'),
+              pattern.draw('plus', 'rgba(255,127,80, 0.6)'),
+              pattern.draw('zigzag', 'rgba(240,230,140, 0.6)'),
+            ],
+            borderColor: [
+              'palegreen',
+              'lightskyblue',
+              'plum',
+              'coral',
+              'khaki',
+            ],
+            hoverBackgroundColor: [
+              pattern.draw('weave', 'palegreen'),
+              pattern.draw('diagonal', 'lightskyblue'),
+              pattern.draw('dot', 'plum'),
+              pattern.draw('plus', 'coral'),
+              pattern.draw('zigzag', 'khaki'),
+            ]
+          },
         ]
       },
       options: {
         legend: {
           display: false,
+        },
+        tooltips: {
+          intersect: false,
+          // titleFontSize: '12',
+          // bodyFontSize: '12',
+        },
+        scales: {
+          xAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              }
+            }
+          ]
         }
       }
     });
   }
 
   render() {
+    const {
+      question,
+      name,
+      choiceNames,
+      votes
+    } = this.props.poll;
+
     return (
       <div className="poll-container">
-        <h2>{this.props.poll.question}</h2>
-        <h3>By: {this.props.poll.name}</h3>
-        <canvas ref={this.chartRef} />
+        <div className="wrapper">
+          <h2>{question}</h2>
+          <h3>By: {name}</h3>
+          <canvas ref={this.chartRef} aria-label="A poll data" role="img"></canvas>
+          <h4>Vote For:</h4>
+          {
+            choiceNames.map((choice, index) => {
+              return (
+                <div key={index}>
+                  <label className="visually-hidden">{choice}: {votes[index]}</label>
+
+                  <button onClick={() => {
+                    this.props.addVote(
+                      this.chart, // passing chart so chart.update() can be used
+                      this.props.id, // the unique key of this particular poll
+                      votes, // the votes array is pass to make access and hencing updating it easier
+                      index // the index of the vote to update
+                    )
+                  }}>
+                    {choice}
+                  </button>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
