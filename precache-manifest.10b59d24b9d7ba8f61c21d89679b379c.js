1ce984c73c37d3992fa6f499dd56b6360ca1924c
jeffreyxdc <jeffreyxdc@gmail.com>
2019-06-03 05:25:42 -0400

    Updates

---
+++ precache-manifest.10b59d24b9d7ba8f61c21d89679b379c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3231e9b98717a542b57650cff3e2354",
+    "url": "/pull-up/index.html"
+  },
+  {
+    "revision": "73cfad0ea40ac6a93cfa",
+    "url": "/pull-up/static/css/main.23e37548.chunk.css"
+  },
+  {
+    "revision": "697e73f1dfbd1c53fbc6",
+    "url": "/pull-up/static/js/2.5101e9de.chunk.js"
+  },
+  {
+    "revision": "73cfad0ea40ac6a93cfa",
+    "url": "/pull-up/static/js/main.a1028a5b.chunk.js"
+  },
+  {
+    "revision": "3c14f584fbe3dbe7e405",
+    "url": "/pull-up/static/js/runtime~main.a6bbdbab.js"
+  }
+]);
